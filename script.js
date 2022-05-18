98d57a6449a5f050e6b7c15592ad279aa1cdb42f
Srivatsan Viswanathan <sriv.viswanathan@gmail.com>
2022-05-17 23:57:36 -0400

    Added Power Function

--- script.js
+++ script.js
@@ -10,8 +10,11 @@ function operate(operator, num1, num2) {
     else if (operator === 'multiply') {
         answer = multiply(num1, num2);
     }
-    else if (operator == 'divide') {
+    else if (operator === 'divide') {
         answer = divide(num1, num2);
+    }
+    else if (operator === 'power') {
+        answer = power(num1, num2);
     }
     else {
         answer = 'Error.';
@@ -21,31 +24,39 @@ function operate(operator, num1, num2) {
 
 // Math functions
 function add(num1, num2) {
-    answer = num1 + num2;
+    let answer = num1 + num2;
     return answer;
 }
 
 function subtract(num1, num2) {
-    answer = num1 - num2;
+    let answer = num1 - num2;
     return answer;
 }
 
 function multiply(num1, num2) {
-    answer = num1 * num2;
+    let answer = num1 * num2;
     return answer;
 }
 
 function divide(num1, num2) {
     if (num2 === 0) {
-        answer = '-_- Really Bruh -_-';
+        let answer = '-_- Really Bruh -_-';
         return answer;
     }
-    answer = num1 / num2;
+    let answer = num1 / num2;
     return answer;
 }
 
-let operator = 'divide';
-let num1 = 2.5;
+function power(num1, num2) {
+    let answer = 1;
+    for (let i = 0; i < num2; i++) {
+        answer = answer*num1;
+    }
+    return answer;
+}
+
+let operator = 'power';
+let num1 = 10;
 let num2 = 0;
 const result = operate(operator, num1, num2);
 console.log(result);
